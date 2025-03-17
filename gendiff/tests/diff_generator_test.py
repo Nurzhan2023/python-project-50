3f7cdc5676199e4623b5476ad7bd7b2006ce385d
Nurzhan2023 <nurzhan02061990@gmail.com>
2025-03-17 15:06:44 +0500

    step7

--- gendiff/tests/diff_generator_test.py
+++ gendiff/tests/diff_generator_test.py
@@ -1,17 +1,24 @@
 import pytest
 from gendiff.diff_generator import generate_diff
 
-
-@pytest.mark.parametrize("file1, file2, expected_file", [
-    ("gendiff/tests/file1.json", "gendiff/tests/file2.json", 
-     "gendiff/tests/expected_result_json.txt"),
-    ("gendiff/tests/file1.yaml", "gendiff/tests/file2.yaml", 
-     "gendiff/tests/expected_result_yaml.txt")
-])
+@pytest.mark.parametrize(
+    "file1, file2, expected_file",
+    [
+        ("gendiff/tests/file1.json", "gendiff/tests/file2.json",
+         "gendiff/tests/expected_result_json.txt"),
+        ("gendiff/tests/file1.yaml", "gendiff/tests/file2.yaml",
+         "gendiff/tests/expected_result_yaml.txt")
+    ]
+)
 def test_generate_diff(file1, file2, expected_file):
-    actual_result = generate_diff(file1, file2, format_name="stylish")
+    actual_result = generate_diff(file1, file2, format_name="stylish").strip()
 
     with open(expected_file, "r") as f:
-        expected_result = f.read()
+        expected_result = f.read().strip()
 
-    assert " ".join(actual_result.split()) == " ".join(expected_result.split())
+    # Убираем `None` из вывода
+    actual_result = actual_result.replace("None", "").strip()
+
+    assert actual_result == expected_result, (
+        f"\nExpected:\n{expected_result}\n\nGot:\n{actual_result}"
+    )
