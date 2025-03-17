813f566d687ee3814c4d936a467554aaa0e69672
Nurzhan2023 <nurzhan02061990@gmail.com>
2025-03-17 12:05:04 +0500

    step7

--- gendiff/tests/diff_generator_test.py
+++ gendiff/tests/diff_generator_test.py
@@ -2,56 +2,16 @@ import pytest
 from gendiff.diff_generator import generate_diff
 
 
-@pytest.mark.parametrize("file1, file2, expected_result", [
-    ("gendiff/tests/file1.json", "gendiff/tests/file2.json", """{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}""")
+@pytest.mark.parametrize("file1, file2, expected_file", [
+    ("gendiff/tests/file1.json", "gendiff/tests/file2.json", "gendiff/tests/expected_result_json.txt"),
+    ("gendiff/tests/file1.yaml", "gendiff/tests/file2.yaml", "gendiff/tests/expected_result_yaml.txt")
 ])
-def test_generate_diff(file1, file2, expected_result):
+def test_generate_diff(file1, file2, expected_file):
     actual_result = generate_diff(file1, file2, format_name="stylish")
 
-    with open("gendiff/tests/expected_result.txt", "w") as f:
-        f.write(actual_result)  # Записываем новый ожидаемый результат
+    # Читаем ожидаемый результат из файла
+    with open(expected_file, "r") as f:
+        expected_result = f.read()
 
+    # Сравниваем результаты, игнорируя лишние пробелы и переносы строк
     assert " ".join(actual_result.split()) == " ".join(expected_result.split())
