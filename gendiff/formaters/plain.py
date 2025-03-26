413c7c9f5c3af061dfc98fd77cbcd37c8afc046c
Nurzhan2023 <nurzhan02061990@gmail.com>
2025-03-26 11:41:51 +0500

    step 8

--- gendiff/formaters/plain.py
+++ gendiff/formaters/plain.py
@@ -7,17 +7,20 @@ def format_plain(diff, path=""):
 
         if status == "added":
             formatted_value = format_value(value["value"])
-            lines.append(f"Property '{property_path}'"
-                         f"was added with value: {formatted_value}")
+            lines.append(f"Property '{property_path}' was added with value: {formatted_value}")
+
         elif status == "removed":
             lines.append(f"Property '{property_path}' was removed")
+
         elif status == "modified":
             old_value = format_value(value["old_value"])
             new_value = format_value(value["new_value"])
-            lines.append(f"Property '{property_path}'"
-                         f" was updated. From {old_value} to {new_value}")
+            lines.append(
+                f"Property '{property_path}' was updated. From {old_value} to {new_value}"
+            )
+
         elif status == "nested":
-            lines.append(format_plain(value["children"], property_path))
+            lines.extend(format_plain(value["children"], property_path).split('\n'))
 
     return "\n".join(lines)
 
---
+++ test.py
@@ -0,0 +1,2 @@
+text = "Property 'common.setting1' was added with value: true"
+print(text)
