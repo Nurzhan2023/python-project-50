6bc905c9752932d80bbb194ef9499196e9e507b8
Nurzhan2023 <nurzhan02061990@gmail.com>
2025-03-26 11:56:37 +0500

    step8

--- gendiff/formaters/plain.py
+++ gendiff/formaters/plain.py
@@ -7,26 +7,29 @@ def format_plain(diff, path=""):
 
         if status == "added":
             formatted_value = format_value(value["value"])
-            lines.append(f"Property '{property_path}' was added with value: {formatted_value}")
+            lines.append(
+                f"Property '{property_path}' "
+                f"was added with value: {formatted_value}"
+            )
 
         elif status == "removed":
             lines.append(f"Property '{property_path}' was removed")
-
         elif status == "modified":
             old_value = format_value(value["old_value"])
             new_value = format_value(value["new_value"])
             lines.append(
-                f"Property '{property_path}' was updated. From {old_value} to {new_value}"
+                f"Property '{property_path}' was updated. "
+                f"From {old_value} to {new_value}"
             )
-
         elif status == "nested":
-            lines.extend(format_plain(value["children"], property_path).split('\n'))
+            lines.extend(
+                format_plain(value["children"], property_path).split('\n')
+            )
 
     return "\n".join(lines)
 
 
 def format_value(value):
-    """Форматирует значение для plain-вывода."""
     if isinstance(value, dict):
         return "[complex value]"
     if isinstance(value, bool):
