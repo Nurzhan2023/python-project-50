a617cea711f85104a8971d95117b916f4be218df
Nurzhan2023 <nurzhan02061990@gmail.com>
2025-03-28 16:55:54 +0500

    step9

--- gendiff/diff_generator.py
+++ gendiff/diff_generator.py
@@ -1,31 +1,34 @@
+# gendiff/diff_generator.py
 from gendiff.read import read_file
 from gendiff.formaters.stylish import format_stylish
 from gendiff.formaters.plain import format_plain
 from gendiff.formaters.json import format_json
 
 
-def get_diff_entry(key, data1, data2):
-    if key not in data1:
-        return {"status": "added", "value": data2[key]}
-    if key not in data2:
-        return {"status": "removed", "value": data1[key]}
-    if isinstance(data1[key], dict) and isinstance(data2[key], dict):
-        return {
-            "status": "nested",
-            "children": build_diff(data1[key], data2[key])
-        }
-    if data1[key] == data2[key]:
-        return {"status": "unchanged", "value": data1[key]}
-    return {
-        "status": "modified",
-        "old_value": data1[key],
-        "new_value": data2[key]
-    }
-
-
 def build_diff(data1, data2):
     keys = sorted(set(data1.keys()) | set(data2.keys()))
-    return {key: get_diff_entry(key, data1, data2) for key in keys}
+    diff = {}
+
+    for key in keys:
+        if key not in data1:
+            diff[key] = {"status": "added", "value": data2[key]}
+        elif key not in data2:
+            diff[key] = {"status": "removed", "value": data1[key]}
+        elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
+            diff[key] = {
+                "status": "nested",
+                "children": build_diff(data1[key], data2[key])
+            }
+        elif data1[key] == data2[key]:
+            diff[key] = {"status": "unchanged", "value": data1[key]}
+        else:
+            diff[key] = {
+                "status": "modified",
+                "old_value": data1[key],
+                "new_value": data2[key]
+            }
+
+    return diff
 
 
 def generate_diff(file_path1, file_path2, format_name="stylish"):
@@ -34,6 +37,7 @@ def generate_diff(file_path1, file_path2, format_name="stylish"):
 
     diff = build_diff(data1, data2)
 
+    # Выбор форматирования
     if format_name == "stylish":
         return format_stylish(diff)
     elif format_name == "plain":
