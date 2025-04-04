e005d21218f74ceaa113163f6f3e5966fbaf15d5
Nurzhan2023 <nurzhan02061990@gmail.com>
2025-04-04 16:43:17 +0500

    update

--- gendiff/formaters/stylish.py
+++ gendiff/formaters/stylish.py
@@ -1,11 +1,12 @@
-# gendiff/formaters/stylish.py
 def format_value(value, depth):
     indent = " " * (depth * 4)
 
     if isinstance(value, dict):
         formatted = ["{"]
         for k, v in value.items():
-            formatted.append(f"{indent}    {k}: {format_value(v, depth + 1)}")
+            formatted.append(
+                f"{indent}    {k}: {format_value(v, depth + 1)}"
+            )
         formatted.append(f"{indent}}}")
         return "\n".join(formatted)
 
@@ -16,40 +17,31 @@ def format_value(value, depth):
     if value is None:
         return "null"
 
-    return str(value)
+    return f" {value}"
 
 
 def format_stylish(diff, depth=0):
     indent = " " * (depth * 4)
     lines = ["{"]
 
-    for key, info in diff.items():
+    for key, info in sorted(diff.items()):
         status = info["status"]
+        value = format_value(info.get("value"), depth + 1)
+        old_value = format_value(info.get("old_value"), depth + 1)
+        new_value = format_value(info.get("new_value"), depth + 1)
 
         if status == "nested":
             children = format_stylish(info["children"], depth + 1)
             lines.append(f"{indent}    {key}: {children}")
         elif status == "added":
-            lines.append(
-                f"{indent}  + {key}: {format_value(info['value'], depth + 1)}"
-            )
+            lines.append(f"{indent}  + {key}: {value}")
         elif status == "removed":
-            lines.append(
-                f"{indent}  - {key}: {format_value(info['value'], depth + 1)}"
-            )
+            lines.append(f"{indent}  - {key}: {value}")
         elif status == "modified":
-            lines.append(
-                f"{indent}  - {key}:"
-                f"{format_value(info['old_value'], depth + 1)}"
-            )
-            lines.append(
-                f"{indent}  + {key}:"
-                f"{format_value(info['new_value'], depth + 1)}"
-            )
+            lines.append(f"{indent}  - {key}: {old_value}")
+            lines.append(f"{indent}  + {key}: {new_value}")
         else:  # unchanged
-            lines.append(
-                f"{indent}    {key}: {format_value(info['value'], depth + 1)}"
-            )
+            lines.append(f"{indent}    {key}: {value}")
 
-    lines.append(indent + "}")
+    lines.append(f"{indent}}}")
     return "\n".join(lines)
